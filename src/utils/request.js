841b556063cc58c654db8ab6f0521a5fc7469e2f
xiefei <liang6.-0@163.com>
2021-01-08 11:48:47 +0800

    init,添加axios配置

---
+++ src/utils/request.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import router from '@/router/index.js'
+
+const service = axios.create({
+  baseURL: '',
+  timeout: 10000,
+  headers: {
+    common: {
+      'X-Requested-With': 'XMLHttpRequest'
+    },
+    post: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  }
+})
+
+
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    if (sessionStorage.getItem("token")) {  // 每次发送请求之前判断是否存在token，如果存在，则统一在http请求的header都加上token，不用每次请求都手动添加了
+        config.headers['Authorization']= sessionStorage.getItem("token");
+    }
+
+    return config
+  },
+  error => {
+    console.log(error)
+    return Promise.reject(error)
+  }
+)
+
+service.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    done = start = 0
+    NProgress.done();
+    if (error.response && +error.response.status === 401) {
+      // alert(1)
+      // window.location.href = error.response.headers.location
+      router.replace({
+          path: '/login',
+          query: {redirect: router.currentRoute.fullPath}
+                    })
+    }
+    return Promise.reject(error)
+  }
+)
+export default service
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    
   </div>
 </template>
+<script>
+//import EmojiPicker from '@/components/EmojiPicker.vue'
+export default {
+   name: 'About'
+  //,
+  // components: {
+  //   EmojiPicker
+  // }
+}
+</script>
